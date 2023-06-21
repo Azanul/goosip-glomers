c0b7de9e167201438c466b2124b89d9a017e0920
Azanul <azanulhaque@gmail.com>
2023-06-21 13:41:47 +0530

    6b Pass
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- maelstrom-txn/main.go
+++ maelstrom-txn/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -20,7 +21,6 @@ func main() {
 	kv := sync.Map{}
 
 	n.Handle("txn", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as an loosely-typed map.
 		var body TxnMessage
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
@@ -45,12 +45,42 @@ func main() {
 			txns = append(txns, crr)
 		}
 
-		// Echo the original message back with the updated message type.
-		fmt.Fprint(os.Stderr, "Received message: ", map[string]any{"type": "txn_ok", "txn": txns}, "\n")
+		go replicate(txns, n)
+
 		return n.Reply(msg, map[string]any{"type": "txn_ok", "txn": txns})
+	})
+
+	n.Handle("sync", func(msg maelstrom.Message) error {
+		var body TxnMessage
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		for _, txn := range body.Transactions {
+			txnType := txn[0].(string)
+			targetKey := txn[1].(float64)
+			if txnType == "w" {
+				kv.Store(string(rune(targetKey)), txn[2].(float64))
+			}
+
+		}
+		return n.Reply(msg, map[string]any{"type": "sync_ok"})
 	})
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func replicate(txns [][3]interface{}, n *maelstrom.Node) {
+	body := map[string]any{
+		"type": "sync",
+		"txn":  txns,
+	}
+	for _, dest := range n.NodeIDs() {
+		_, err := n.SyncRPC(context.TODO(), dest, body)
+		if err != nil {
+			fmt.Fprint(os.Stderr, "Error message: ", err, "\n")
+		}
 	}
 }
